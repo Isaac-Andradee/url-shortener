083411e9aa09df93f15f8c8d6484431b798c96c2
Isaac-Andradee <isaac.andra84@gmail.com>
2025-05-10 17:59:01 -0300

    init: Initial commit

---
+++ src/test/java/com/isaacandrade/url_shortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaacandrade.url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
