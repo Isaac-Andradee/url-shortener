7ad16c4170ac7a2cd050f49f71c4c71d32b530bf
Isaac-Andradee <isaac.andra84@gmail.com>
2025-06-29 02:48:28 -0300

    feat: adding actuator helth check for services

--- shortener-service/src/main/java/com/isaacandrade/urlshortenerservice/config/EurekaClientConfig.java
+++ shortener-service/src/main/java/com/isaacandrade/urlshortenerservice/config/EurekaClientConfig.java
@@ -1,7 +1,26 @@
+/*
+ * Copyright 2024 the original author.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.isaacandrade.urlshortenerservice.config;
 
+import com.netflix.appinfo.InstanceInfo;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaHealthCheckHandler;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,15 +28,18 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.ConfigurableEnvironment;
 
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Collections;
-import java.util.Enumeration;
 import java.util.Optional;
 
+/**
+ * Configuration class for Eureka client in the shortener service.
+ *
+ * @author Isaac Andrade
+ */
 @Configuration
 @EnableConfigurationProperties
 @Profile("!test")
@@ -26,6 +48,17 @@ public class EurekaClientConfig {
 
     public EurekaClientConfig(final ConfigurableEnvironment env) {
         this.env = env;
+    }
+
+    @Bean
+    public EurekaHealthCheckHandler healthCheckHandler(HealthEndpoint healthEndpoint) {
+        return new EurekaHealthCheckHandler(statuses -> healthEndpoint.health().getStatus()) {
+            @Override
+            public InstanceInfo.InstanceStatus getStatus(InstanceInfo.InstanceStatus currentStatus) {
+                Status status = healthEndpoint.health().getStatus();
+                return status.equals(Status.UP) ? InstanceInfo.InstanceStatus.UP : InstanceInfo.InstanceStatus.DOWN;
+            }
+        };
     }
 
     @Bean
