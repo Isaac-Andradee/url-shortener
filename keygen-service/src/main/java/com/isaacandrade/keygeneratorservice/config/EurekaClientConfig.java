3bc47a6d04a8b928366b0cf2ef1c87392d49af8b
Isaac-Andradee <isaac.andra84@gmail.com>
2025-06-30 01:07:13 -0300

    license: warning about license

--- keygen-service/src/main/java/com/isaacandrade/keygeneratorservice/config/EurekaClientConfig.java
+++ keygen-service/src/main/java/com/isaacandrade/keygeneratorservice/config/EurekaClientConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 the original author.
+ * Copyright 2025 the original author.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
